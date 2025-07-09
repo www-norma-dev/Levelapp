14ba5417b290e8cc17c7f98d7cc9ea55e6f5d59e
AnasAmchaar <anass.amchaar14@gmail.com>
2025-07-09 11:01:39 +0100

    removing relativeDateOption class

--- level-core/simluators/schemas.py
+++ level-core/simluators/schemas.py
@@ -18,12 +18,3 @@ class InteractionEvaluationResult(BaseModel):
     extracted_metadata_evaluation: float
     scenario_id: str
 
-class RelativeDateOption(str, Enum):
-    """Enum representing the relative date options."""
-    TODAY_PLUS_7 = "today plus 7"
-    IN_1_MONTH = "in 1 month"
-    IN_2_MONTHS = "in 2 months"
-    IN_3_MONTHS = "in 3 months"
-    IN_4_MONTHS = "in 4 months"
-    TODAY = "today"
-    TOMORROW = "tomorrow" 
