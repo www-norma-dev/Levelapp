b3a6a98c8b1bcc3fedd198aef365ae03b3b84247
AnasAmchaar <anass.amchaar14@gmail.com>
2025-07-24 11:07:10 +0100

    cleaning the commented part of the ConversationSimulator class

--- level_core/simluators/service.py
+++ level_core/simluators/service.py
@@ -13,7 +13,6 @@ from .utils import (
     parse_date_value,
     calculate_average,
     summarize_justifications,
-    calculate_handoff_stats,
 )
 from .event_collector import add_event
 from level_core.simluators.schemas import ConversationBatch
@@ -64,24 +63,24 @@ class ConversationSimulator:
             Dict[str, Any]: The test results with status information.
         """
         add_event("INFO", f"Starting batch test for batch: {name}")
-        # started_at = datetime.now().isoformat()
-        # start_time = time.time()
+        started_at = datetime.now().isoformat()
+        start_time = time.time()
         results = await self.simulate_conversation(attempts=attempts)
-        # finished_at = datetime.now().isoformat()
-        # elapsed_time = time.time() - start_time
-        # average_execution_time = calculate_average(results["scenarios"])
-        # test_load["results"] = {
-        #     "started_at": started_at,
-        #     "finished_at": finished_at,
-        #     "total_duration_seconds": elapsed_time,
-        #     "global_justification": self.evaluation_summaries,
-        #     "average_scores": results["average_scores"],
-        #     "scenarios": results["scenarios"],
-        #     "average_execution_time": average_execution_time,
-        #     "execution_events": self.execution_events,  # (Legacy) - consider using event_collector.execution_events
-        # }
-        # if self.persistence_fn:
-        #     self.persistence_fn(test_load)
+        finished_at = datetime.now().isoformat()
+        elapsed_time = time.time() - start_time
+        average_execution_time = calculate_average(results["scenarios"])
+        test_load["results"] = {
+            "started_at": started_at,
+            "finished_at": finished_at,
+            "total_duration_seconds": elapsed_time,
+            "global_justification": self.evaluation_summaries,
+            "average_scores": results["average_scores"],
+            "scenarios": results["scenarios"],
+            "average_execution_time": average_execution_time,
+            "execution_events": self.execution_events,  # (Legacy) - consider using event_collector.execution_events
+        }
+        if self.persistence_fn:
+            self.persistence_fn(test_load)
         return results
 
     async def simulate_conversation(self, attempts: int = 1) -> Dict[str, Any]:
@@ -110,14 +109,7 @@ class ConversationSimulator:
             self.evaluation_summaries[provider] = summarize_justifications(
                 justifications=justifications
             )
-        # all_handoff_vals: List[float] = []
-        # for scen in results:
-        #     for attempt in scen.get("attempts", []):
-        #         val = attempt.get("handoffPassAverage")
-        #         if isinstance(val, (int, float)):
-        #             all_handoff_vals.append(val)
-        # handoff_avg_map = calculate_average({"handoff": all_handoff_vals})
-        # overall_average_scores["handoff"] = handoff_avg_map.get("handoff", 0.0)
+
         return {
             "scenarios": results,
             "average_scores": overall_average_scores,
@@ -142,32 +134,20 @@ class ConversationSimulator:
             add_event("INFO", f"Running attempt: {attempt+1}/{attempts}", {"scenario_id": scenario_id})
             start_time = time.time()
             self.collected_scores = defaultdict(list)
-            # attempt_handoff_checks: List[int] = []
             conversation_id = f"batch-{attempt+1}"
-            # initial_interaction_results = await self.simulate_initial_interaction(
-            #     scenario=scenario,
-            #     conversation_id=conversation_id
-            # )
-            # attempt_handoff_checks.append(initial_interaction_results.get("handoffPassCheck", 0))
             interactions_results = await self.simulate__interactions(
                 scenario=scenario,
                 conversation_id=conversation_id
             )
-            # for r in inbound_interactions_results:
-                # if "handoffPassCheck" in r:
-                #     attempt_handoff_checks.append(r["handoffPassCheck"])
-                # pass 
+
             single_attempt_scores = calculate_average(self.collected_scores)
             for key in all_attempts_scores:
                 all_attempts_scores[key].append(single_attempt_scores)
-            # elapsed_time = time.time() - start_time
-            # handoff_stats = calculate_handoff_stats(attempt_handoff_checks)
-            # attempt_handoff_average = handoff_stats["average"]
-            # all_attempts_scores["handoff"].append(attempt_handoff_average)
+
             attempt_results.append({
                 "attempt_id": attempt + 1,
                 "conversation_id": conversation_id,
-                "interactions": interactions_results,  # renamed from inbound_interactions
+                "interactions": interactions_results, 
                 "average_scores": single_attempt_scores,
                 "execution_time": f"{time.time() - start_time:.2f}",
             })
@@ -178,74 +158,6 @@ class ConversationSimulator:
             "attempts": attempt_results,
             "average_scores": average_scores,
         }
-
-    # async def simulate_initial_interaction(self, scenario: Any, conversation_id: str) -> Dict[str, Any]:
-    #     """
-    #     Simulate the initial interaction for a scenario.
-
-    #     Args:
-    #         scenario (Any): The scenario to simulate.
-    #         conversation_id (str): The conversation ID.
-
-    #     Returns:
-    #         Dict[str, Any]: The results of the initial interaction simulation.
-    #     """
-    #     add_event("INFO", "Starting initial interaction simulation..")
-    #     start_time = time.time()
-    #     initial_interaction = getattr(scenario, 'initial_interaction', None)
-    #     if initial_interaction is None:
-    #         return {}
-    #     initial_interaction.conversation_id = conversation_id
-    #     expected_metadata = getattr(scenario, 'expected_metadata', {})
-    #     # User must supply serialization logic for their scenario objects
-    #     payload = getattr(initial_interaction, 'to_dict', lambda: dict())()
-    #     response = await async_request(
-    #         url=self.endpoint,
-    #         headers=self.headers,
-    #         payload=payload,
-    #     )
-    #     if not response or not response.status_code == 200:
-    #         add_event("ERROR", "Initial interaction request failed.", {
-    #             "status_code": response.status_code if response else "No response",
-    #             "conversation_id": conversation_id
-    #         })
-    #         return {
-    #             "userMessage": getattr(initial_interaction, 'message', None),
-    #             "reply": "Request failed",
-    #             "expectedReply": getattr(scenario, 'expected_initial_reply', None),
-    #             "extractedMetadata": {},
-    #             "expectedMetadata": expected_metadata,
-    #             "handoffDetails": None,
-    #             "interactionType": "",
-    #             "evaluationResults": {},
-    #             "handoffPassCheck": 0,
-    #         }
-    #     interaction_details = extract_interaction_details(response_text=response.text)
-    #     evaluation_results = None
-    #     if self.evaluation_fn:
-    #         evaluation_results = self.evaluation_fn(
-    #             extracted_reply=interaction_details.reply,
-    #             reference_reply=getattr(scenario, 'expected_initial_reply', None),
-    #             extracted_metadata=interaction_details.extracted_metadata,
-    #             reference_metadata=expected_metadata,
-    #             scenario_id=getattr(scenario, 'scenario_id', 'unknown')
-    #         )
-    #     # actual_handoff = interaction_details.handoff_details is not None
-    #     # expected_handoff = getattr(initial_interaction, 'expected_handoff_pass', False)
-    #     # handoff_pass_check = 1 if expected_handoff == actual_handoff else 0
-    #     elapsed_time = time.time() - start_time
-    #     return {
-    #         "userMessage": getattr(initial_interaction, 'message', None),
-    #         "reply": interaction_details.reply,
-    #         "expectedReply": getattr(scenario, 'expected_initial_reply', None),
-    #         "extractedMetadata": interaction_details.extracted_metadata,
-    #         "expectedMetadata": expected_metadata,
-    #         "handoffDetails": interaction_details.handoff_details,
-    #         "interactionType": interaction_details.interaction_type,
-    #         "evaluationResults": evaluation_results,
-    #         "handoffPassCheck": 0, # Commented out handoff logic
-    #         "executionTime": f"{elapsed_time:.2f}",
-    #     }
 
     async def simulate__interactions(self, scenario: BasicConversation, conversation_id: str) -> List[Dict[str, Any]]:
         """
@@ -286,21 +198,8 @@ class ConversationSimulator:
                 results.append(result)
                 continue
     
-            # interaction_details = extract_interaction_details(response_text=response.text)
             evaluation_results= await self.evaluate_interaction(response.text, interaction.reference_reply)
-            # evaluation_results = None
-            # if self.evaluation_fn:
-            #     evaluation_results = self.evaluation_fn(
-            #         extracted_reply=response.text,
-            #         reference_reply=getattr(interaction, 'reference_reply', None),
-            #         generated_metadata= {},
-            #         reference_metadata=getattr(interaction, 'reference_metadata', {}),
-            #         scenario_id=getattr(scenario, 'scenario_id', 'unknown')
-            #     )
 
-            # actual_handoff = interaction_details.handoff_details is not None
-            # expected_handoff = getattr(interaction, 'expected_handoff_pass', False)
-            # handoff_pass_check = 1 if expected_handoff == actual_handoff else 0
             result = {
                 "user_message": interaction.user_message,
                 "agent_reply": response.text,
@@ -347,40 +246,7 @@ class ConversationSimulator:
 
         openai_reply_evaluation, ionos_reply_evaluation = await asyncio.gather(openai_eval_task, ionos_eval_task)
 
-        # extracted_metadata_evaluation = evaluate_metadata(
-        #     expected=reference_metadata,
-        #     actual=extracted_metadata,
-        #     field_types={}
-        # )
-
         return {
             "openai": openai_reply_evaluation,
             "ionos": ionos_reply_evaluation,
         }
-
-        # return InteractionEvaluationResult(
-        #     openaiReplyEvaluation=openai_reply_evaluation,
-        #     ionosReplyEvaluation=ionos_reply_evaluation,
-        #     extractedMetadataEvaluation=extracted_metadata_evaluation,
-        #     scenarioTitle=scenario_title
-        # )
-
-    # def store_evaluation_results(self, results: InteractionEvaluationResult) -> None:
-    #     """
-    #     Store the evaluation results in the evaluation summary.
-
-    #     Args:
-    #         results (InteractionEvaluationResult): The evaluation results to store.
-    #     """
-    #     self.evaluation_results["openaiJustificationSummary"].append({
-    #         "scenario": results.scenarioTitle,
-    #         "justification": results.openaiReplyEvaluation.justification
-    #     })
-    #     self.evaluation_results["ionosJustificationSummary"].append({
-    #         "scenario": results.scenarioTitle,
-    #         "justification": results.ionosReplyEvaluation.justification
-    #     })
-        
-    #     self.collected_scores["openai"].append(results.openaiReplyEvaluation.match_level)
-    #     self.collected_scores["ionos"].append(results.ionosReplyEvaluation.match_level)
-    #     self.collected_scores["metadata"].append(results.extractedMetadataEvaluation)
